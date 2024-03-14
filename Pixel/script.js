0685b49429a61ad441e67b2f3819f0352c8cdb51
Dilber Şah <114154277+sahdilber@users.noreply.github.com>
2024-03-14 17:36:10 +0300

    düzeltmeler

--- Pixel/script.js
+++ Pixel/script.js
@@ -53,6 +53,15 @@ function showMovies(movies){
     })
 }
 
+function getClassByRate(vote){
+    if(vote>=8){
+        return 'green'
+    } else if(vote>=5){
+        return 'orange'
+    }else{
+        return 'red'
+    }
+}
 
 sform.addEventListener('submit',(e)=>{
     e.preventDefault()
