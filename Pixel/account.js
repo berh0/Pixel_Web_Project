60b9b6a260dd63354251b499c926a386f69e4668
Dilber Şah <114154277+sahdilber@users.noreply.github.com>
2024-03-17 19:00:31 +0300

    register eklendi

--- Pixel/account.js
+++
@@ -1,9 +0,0 @@
-
-const src=document.querySelector('.src')
-const btn=document.querySelector('.btn')
-
-btn.addEventListener('click',()=>{
-    src.classList.toggle('active')
-    search.focus()
-})
-
